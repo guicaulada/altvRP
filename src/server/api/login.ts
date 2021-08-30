71546e96333a67a3a5d1508631df52ce8e4039df
Sighmir <guilherme.caulada@gmail.com>
2021-08-30 02:26:30 -0300

    Fix issue with bad requests on /authorize

--- src/server/api/login.ts
+++ src/server/api/login.ts
@@ -27,19 +27,23 @@ const authPlayer = (state: AuthState) => {
 };
 
 router.get("/authorize", async (req, res) => {
-  const state = JSON.parse(decrypt(req.query.state as string));
-  const token = await getToken(req.query.code as string);
-  const user = await getUser(token.access_token);
-  const player = authPlayer(state);
-  if (player) {
-    console.log(user);
-    console.log(state);
-    proxy.client.closeLogin(player);
-    player.spawn(-695.1956176757812, 83.94725036621094, 55.85205078125);
-    player.model = "a_c_chimp";
-    player.giveWeapon(0xaf113f99, 1000000, true);
-    res.status(200).send();
-  } else {
+  try {
+    const state = JSON.parse(decrypt(req.query.state as string));
+    const player = authPlayer(state);
+    if (state && player) {
+      const token = await getToken(req.query.code as string);
+      const user = await getUser(token.access_token);
+      console.log(user);
+      console.log(state);
+      proxy.client.closeLogin(player);
+      player.spawn(-695.1956176757812, 83.94725036621094, 55.85205078125);
+      player.model = "a_c_chimp";
+      player.giveWeapon(0xaf113f99, 1000000, true);
+      res.status(200).send();
+    } else {
+      res.status(401).send();
+    }
+  } catch {
     res.status(401).send();
   }
 });
